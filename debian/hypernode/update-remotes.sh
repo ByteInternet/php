ac2a77204fca6f513d80b224da373683bdff5b8c
Allard Hoeve <allardhoeve@gmail.com>
2016-08-30 16:39:16 +0200

    Add hypernode-update-remotes.sh

---
+++ debian/hypernode/update-remotes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+GIT="chronic git"
+HYPERNODE_BRANCH="hypernode-7.0"
+MAINTAINER_BRANCHES="master-7.0 upstream-7.0 pristine-tar"
+
+$GIT reset --hard
+$GIT clean -fd
+
+if ! (git remote -v | grep pkg-php > /dev/null); then
+    echo "It seems like you do not have the remote pkg-php sources configured."
+    echo "I will now do that for you"
+
+    $GIT remote add pkg-php https://github.com/pkg-php/php.git
+    $GIT remote set-branches pkg-php $MAINTAINER_BRANCHES
+fi
+
+echo "Will now refresh maintainer's branches for you: $MAINTAINER_BRANCHES"
+$GIT fetch pkg-php
+
+echo "Resetting your local branches to their respective maintainer's branches"
+
+for branch in $MAINTAINER_BRANCHES; do
+    echo -n " - Updating $branch: "
+
+    if ! $GIT checkout $branch; then
+        echo "    - Creating local branch $branch off pkg-php/$branch"
+        $GIT branch $branch pkg-php/$branch
+        $GIT checkout $branch
+    else
+        echo "done"
+    fi
+    
+    $GIT clean -fd
+    $GIT reset --hard pkg-php/$branch
+    $GIT push byte $branch
+done
+    
+$GIT checkout hypernode-7.0
+
+echo "Now merge master-7.0 to hypernode-7.0 to merge new changes"
